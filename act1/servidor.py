33d1e89ad8a7845c0a522a0686f6922753a584be
rorrodrigo <rorrodrigo.-@hotmail.com>
2019-09-30 00:45:14 -0300

    Add files via upload

---
+++ act1/servidor.py
@@ -0,0 +1,44 @@
+import socket
+import os.path
+
+log = "log.txt"
+if (os.path.isfile(log)):
+    archivo = open(log,"a")
+else:
+    archivo = open(log,"w")
+
+#instanciamos un objeto para trabajar con el socket
+ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+#Puerto y servidor que debe escuchar
+ser.bind(("localhost", 5000))
+
+flag = True 
+while flag:
+    #Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexiones simutáneas.
+    ser.listen(1)
+    print("Esperando conexiones...")
+
+    #Instanciamos un objeto cli (socket cliente) para recibir datos
+    cli, addr = ser.accept()
+
+    while True:
+        #Recibimos el mensaje
+        recibido = cli.recv(1024)
+        #Si se reciben datos nos muestra la IP y el mensaje recibido
+        print ("IP: " + str(addr[0]) + " Mensaje:" + recibido.decode())
+        archivo.write("IP: " + str(addr[0]) + "| Mensaje:" + recibido.decode()+"\n")
+
+        if(recibido.decode() == "salir"):
+            cli.close()
+            break
+        elif(recibido.decode() == "cerrar servidor"):
+            cli.close()
+            ser.close()
+            flag = False
+            break
+        #Devolvemos el mensaje al cliente
+        cli.send("Mensaje recibido".encode())
+
+archivo.close()
+print("Servidor finalizado")
